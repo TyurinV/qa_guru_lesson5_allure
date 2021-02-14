343281ce9e35cca43da149f17788ba2b30ec4169
TyurinV <denfal486@gmailc.om>
2021-02-14 20:32:40 +0300

    Test is ready for allure

---
+++ src/test/java/tests/SimleTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SimleTest {
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+    @Test
+    void shouldHaveIssueInGithub() {
+        //Открыть страницу Github
+        open("https://github.com/");
+        // Открыть Selenide
+        $("[name=q]").setValue("allure").pressEnter();
+        $(".repo-list li a").click();
+        $("[data-tab-item=i1issues-tab]").click();
+        $("#issue_1183_link").shouldHave(text("Attach external html file to the test run"));
+
+
+}}
