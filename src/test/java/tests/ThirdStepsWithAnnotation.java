b5192c77643bbbacb5988a5bb16b7ac8bf54d8c5
TyurinV <denfal486@gmailc.om>
2021-02-15 22:19:32 +0300

    Three methods are ready

--- src/test/java/tests/ThirdStepsWithAnnotation.java
+++ src/test/java/tests/ThirdStepsWithAnnotation.java
@@ -1,13 +1,15 @@
 package tests;
 
-import io.qaguru.TyurinV.BaseTest;
+import io.qaguru.TyurinV.BaseSteps;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ThirdStepsWithAnnotation {
 
-        public BaseTest steps = new BaseTest();
+        public BaseSteps steps = new BaseSteps();
 
         @Test
+        @DisplayName("Поиск issue - Attach external html file to the test run")
         public void testIssueCheck(){
             steps.openMainPage();
             steps.allureSearch();
